1dfa9c748f4c795487c5cfb32ac78bf716cdef5d
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-10-10 20:18:00 -0500

    add global_nav__lg and globa__header

--- src/assets/component/Navigation.jsx
+++ src/assets/component/Navigation.jsx
@@ -3,22 +3,25 @@ import logo from '../images/shared/desktop/logo.svg'
 import cart from '../images/shared/desktop/icon-cart.svg'
 import { Link } from 'react-router-dom'
 
-const global__nav = 'bg-black-100 px-[6.4vw] py-[8.54vw]'
+const global__nav = 'px-[6.4vw] py-[8.54vw]'
 const global__nav__ul = 'flex items-center'
 const global__nav__li = 'm-auto'
 
 const global__nav__md = 'md:px-[5.20vw] md:py-[4.16vw] '
 const global__nav__li__md = 'md:ml-[42px] md:mr-auto'
 
+const global__nav__lg = 'lg:pt-[2.23vw] lg:pb-[2.5vw] lg:px-0 lg:mx-auto lg:w-[77.09vw]'
+const global__nav__li__lg = 'lg:m-auto'
+
 const Navigation = () => {
   return (
     <>
-      <nav className={`${global__nav} ${global__nav__md}`}>
+      <nav className={`${global__nav} ${global__nav__md} ${global__nav__lg}`}>
         <ul className={`${global__nav__ul}`}>
           <li>
             <img src={hamburguer} alt="hamburguer" />
           </li>
-          <li className={`${global__nav__li} ${global__nav__li__md}`}>
+          <li className={`${global__nav__li} ${global__nav__li__md} ${global__nav__li__lg}`}>
             <Link to={'/'}>
               <img src={logo} alt="logo" />
             </Link>
--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Navigation from '../component/Navigation'
 
+const global__header = 'bg-black-100'
+
 export default function Home() {
   return (
     <>
-      <header>
+      <header className={global__header}>
         <Navigation />
       </header>
     </>
