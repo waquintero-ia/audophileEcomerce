a7d2f3f4faec60a36a848fa347b92d6410889c1b
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-10-10 19:36:51 -0500

    add navigation component

--- src/assets/component/Navigation.jsx
+++ src/assets/component/Navigation.jsx
@@ -3,15 +3,19 @@ import logo from '../images/shared/desktop/logo.svg'
 import cart from '../images/shared/desktop/icon-cart.svg'
 import { Link } from 'react-router-dom'
 
+const global__nav = 'bg-black-100 px-[6.4vw] py-[8.54vw]'
+const global__nav__ul = 'flex items-center'
+const global__nav__li = 'm-auto'
+
 const Navigation = () => {
   return (
     <>
-      <nav className='bg-black-100 px-[6.4vw] py-[8.54vw]'>
-        <ul className='flex justify-between items-center'>
+      <nav className={`${global__nav}`}>
+        <ul className={`${global__nav__ul}`}>
           <li>
             <img src={hamburguer} alt="hamburguer" />
           </li>
-          <li>
+          <li className={`${global__nav__li}`}>
             <Link to={'/'}>
               <img src={logo} alt="logo" />
             </Link>
