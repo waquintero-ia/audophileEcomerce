9735d8c0a8f3fb8a7cfcde139b1b3b7590c1f9db
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-10-10 19:47:31 -0500

    fixed global__nav__md and global__nav__li__me

--- src/assets/component/Navigation.jsx
+++ src/assets/component/Navigation.jsx
@@ -7,18 +7,18 @@ const global__nav = 'bg-black-100 px-[6.4vw] py-[8.54vw]'
 const global__nav__ul = 'flex items-center'
 const global__nav__li = 'm-auto'
 
-const global__nav__lg = 'lg:px-[5.20vw] lg:py-[4.16vw] '
-const global__nav__li__lg = 'lg:ml-[42px] lg:mr-auto'
+const global__nav__md = 'md:px-[5.20vw] md:py-[4.16vw] '
+const global__nav__li__md = 'md:ml-[42px] md:mr-auto'
 
 const Navigation = () => {
   return (
     <>
-      <nav className={`${global__nav} ${global__nav__lg}`}>
+      <nav className={`${global__nav} ${global__nav__md}`}>
         <ul className={`${global__nav__ul}`}>
           <li>
             <img src={hamburguer} alt="hamburguer" />
           </li>
-          <li className={`${global__nav__li} ${global__nav__li__lg}`}>
+          <li className={`${global__nav__li} ${global__nav__li__md}`}>
             <Link to={'/'}>
               <img src={logo} alt="logo" />
             </Link>
