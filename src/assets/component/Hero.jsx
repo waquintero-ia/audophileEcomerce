97fafc05fda631498f09c4b6ba5e4dfdd451e80a
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-10-13 23:59:03 -0500

    fixed hero component

--- src/assets/component/Hero.jsx
+++ src/assets/component/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   const global__hero = 'text-white flex flex-col justify-center items-center bg-black-100 px-[6.4vw] pt-[28.8vw] pb-[29.87vw]'
   const global__subtitle = 'text-14-sm tracking-[0.625rem] opacity-[49.64%]'
   const global__title = 'text-36-sm font-bold leading-[110%] tracking-[0.080625rem]'
-  const global__description = 'text-15-sm font-medium leading-[160%]'
+  const global__description = 'text-15-sm font-medium leading-[160%] opacity-[75%]'
 
   const utils__uppercase = 'uppercase'
   const utils__text__center = 'text-center'
