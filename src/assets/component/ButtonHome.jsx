3c4a8aab92ba61f9084d1d37273f1894631fb507
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-10-14 01:41:24 -0500

    fixed hero component desktop

--- src/assets/component/ButtonHome.jsx
+++ src/assets/component/ButtonHome.jsx
@@ -1,11 +1,12 @@
 const ButtonHome = () => {
 
   const global__button = 'uppercase bg-orange-100 w-[42.67vw] min-h-[12.8vw] text-13-sm font-bold tracking-[0.0625rem]'
-  const global__button__md = 'md:text-13-md md:w-[20.84vw] min-h-[6.25vw]'
+  const global__button__md = 'md:text-13-md md:w-[20.84vw] md:min-h-[6.25vw]'
+  const global__button__xl = 'xl:text-13-xl xl:w-[11.12vw] xl:min-h-[3.34vw]'
 
   return (
     <>
-      <button type="button" className={`${global__button} ${global__button__md}`}>
+      <button type="button" className={`${global__button} ${global__button__md} ${global__button__xl}`}>
         See product
       </button>
     </>
--- src/assets/component/Hero.jsx
+++ src/assets/component/Hero.jsx
@@ -4,30 +4,37 @@ const Hero = () => {
 
   const global__hero = 'text-white flex flex-col justify-center items-center bg-black-100 px-[6.4vw] pt-[28.8vw] pb-[29.87vw]'
   const global__hero__md = 'md:px-0 md:pt-[16.41vw] md:pb-[21.75vw]'
+  const global__hero__xl = 'xl:justify-start xl:items-start xl:px-[11.46vw] xl:pt-[8.89vw] xl:pb-[10.98vw]'
   const global__container__hero = 'flex flex-col justify-center items-center'
+  const global__container__hero__md = 'md:w-[45.44vw]'
+  const global__container__hero__xl = 'xl:w-[27.5vw]'
   const global__subtitle = 'text-14-sm tracking-[0.625rem] opacity-[49.64%]'
   const global__subtitle__md = 'md:text-14-md'
+  const global__subtitle_xl = 'xl:text-14-xl'
   const global__title = 'text-36-sm font-bold leading-[110%] tracking-[0.080625rem]'
   const global__title__md = 'md:text-56-md md:leading-[100%] tracking-[0.125rem]'
+  const global__title_xl = 'xl:text-56-xl xl:leading-[105%]'
   const global__description = 'text-15-sm font-medium leading-[160%] opacity-[75%]'
-  const global__description__md = 'md:text-15-md md:w-[349px md:leading-[170%] w-[45.45vw]'
+  const global__description__md = 'md:text-15-md md:leading-[170%]'
+  const global__description_xl = 'xl:text-15-xl xl:leading-[154%]'
 
   const utils__uppercase = 'uppercase'
   const utils__text__center = 'text-center'
+  const utils__text_left__xl = 'xl:text-left'
 
   return (
     <>
-      <article className={`hero flow__hero ${global__hero} ${global__hero__md}`}>
-        <div className={`container_hero flow__hero__description ${global__container__hero}`}>
+      <article className={`hero flow__hero ${global__hero} ${global__hero__md} ${global__hero__xl}`}>
+        <div className={`container_hero flow__hero__description ${global__container__hero} ${global__container__hero__md} ${global__container__hero__xl}`}>
           <div className="container__title flow__hero__title">
-            <p className={`subtitle ${global__subtitle} ${global__subtitle__md} ${utils__uppercase} ${utils__text__center}`}>
+            <p className={`subtitle ${global__subtitle} ${global__subtitle__md} ${global__subtitle_xl} ${utils__uppercase} ${utils__text__center} ${utils__text_left__xl}`}>
               New products
             </p>
-            <h1 className={`title ${global__title} ${global__title__md} ${utils__uppercase} ${utils__text__center}`}>
+            <h1 className={`title ${global__title} ${global__title__md} ${global__title_xl} ${utils__uppercase} ${utils__text__center} ${utils__text_left__xl}`}>
               XX99 Mark II Headphones
             </h1>
           </div>
-          <p className={`description ${global__description} ${global__description__md} ${utils__text__center}`}>
+          <p className={`description ${global__description} ${global__description__md} ${global__description_xl} ${utils__text__center} ${utils__text_left__xl}`}>
             Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.
           </p>
         </div>
